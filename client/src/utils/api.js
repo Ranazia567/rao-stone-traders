7c8ecb70edb00eacd69a63189a2d44b5766a4779
Zia <zia@movieapp.com>
2026-09-22 22:33:05 +0500

    Add production deployment configurations

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE = '/api';
+const API_BASE = import.meta.env.VITE_API_URL || '/api';
 
 const request = async (endpoint, options = {}) => {
   const res = await fetch(`${API_BASE}${endpoint}`, options);
